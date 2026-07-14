0881256cb405cb80abb2cd37846f6f814f20311e
DanielRM2 <danjod2004@gmail.com>
2026-07-13 20:09:10 -0500

    actualizacion

--- db.js
+++ db.js
@@ -1,19 +1,21 @@
-const mysql = require("mysql2");
+const { Client } = require('pg');
 
-const connection = mysql.createConnection({
-  host: "hayabusa.proxy.rlwy.net",
-  port: 44512,
-  user: "root",
-  password: "klILPHjjCuwYonuXEJiqpyxHJcRmPlpl",
-  database: "railway",
+// Reemplaza el string de abajo con el 'External Database URL' de Render
+const connectionString = "postgresql://dbrosatel_user:cIECNB40sr6Va3GTsqBuBDekt8inXtHU@dpg-d9aohpjtqb8s739l0lg0-a.oregon-postgres.render.com/dbrosatel";
+
+const client = new Client({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false // Esto es obligatorio para conectarse a Render desde afuera
+  }
 });
 
-connection.connect((err) => {
+client.connect((err) => {
   if (err) {
-    console.error("Error conectando a la base de datos:", err);
+    console.error("Error conectando a la base de datos PostgreSQL:", err.stack);
     return;
   }
-  console.log("Conectado a Railway MySQL");
+  console.log("Conectado exitosamente a PostgreSQL en Render");
 });
 
-module.exports = connection;
+module.exports = client;
